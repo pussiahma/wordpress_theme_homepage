85ffac279418ae278b7abb05eb3e32953b79732b
pussiahma <ella.ruokokoski@gmail.com>
2016-11-07 17:27:56 +0200

    changed file

--- js/kotisivu.js
+++ js/kotisivu.js
@@ -32,30 +32,20 @@ $('.bubble').fadeOut(2000);
 var $overlay = $('<div id="overlay"></div>');
 var $image = $("<img>");
 
-
-//An image to overlay
 $overlay.append($image);
 
-//Add overlay
 $("#metaslider_container_78").append($overlay);
-
-//Capture the click event on a link to an image
 $("#metaslider_78 a").click(function(event){
   event.preventDefault();
   var imageLocation = $(this).attr("href");
-  //Update overlay with the image linked in the link
   $image.attr("src", imageLocation);
-  
-  //Show the overlay.
   $overlay.show();
   $("body").css(  "background", "rgba(0,0,0,0.7)");
-  //Get child's alt attribute and set caption
 
 });
 
-//When overlay is clicked
+
 $overlay.click(function(){
-  //Hide the overlay
   $overlay.fadeOut();
     $("body").css(  "background", "#fff");
 
